2d0db9249090ab99a5bfcf30a881484d447939ef
SHARP <ssharp9999@gmail.com>
2021-09-17 17:57:01 -0700

    category route init

---
+++ routes/category-routes.js
@@ -0,0 +1,32 @@
+// require express to use express module
+const router = require('express').Router()
+// require Category and Product models
+const { Category, Product } = require('../models')
+
+// The `/api/categories` endpoint
+
+// get all categories
+router.get('/categories', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+})
+
+router.get('/categories/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+})
+
+router.post('/categories', (req, res) => {
+  // create a new category
+})
+
+router.put('/categories/:id', (req, res) => {
+  // update a category by its `id` value
+})
+
+router.delete('/categories/:id', (req, res) => {
+  // delete a category by its `id` value
+})
+
+// export router for use
+module.exports = router
